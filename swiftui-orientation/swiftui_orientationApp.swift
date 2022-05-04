fec9ce3363d4d6bfc25713abd3f81dc9c93de269
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-05-04 04:12:27 -0500

    Initial Commit

---
+++ swiftui-orientation/swiftui_orientationApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_orientationApp.swift
+//  swiftui-orientation
+//
+//  Created by Mark Volkmann on 5/4/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_orientationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
